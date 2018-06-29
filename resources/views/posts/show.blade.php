5776b815b3150f5e393ece08916ea985c8444fb8
kamilmatnoor <kamilmatnoor@gmail.com>
2018-06-29 16:39:47 +0800

    Added feature to create new post, Added laravel/collective package to support HTML element, Added article-ckeditor for textarea

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <a href='/posts' class='btn btn-info'>Back</a>
     <h1>{{$post->title}}</h1>
-    <div>{{$post->body}}</div>
+    <div>{!!$post->body!!}</div>
     <hr>
     <small>Written on {{$post->created_at}}</small>
 @endsection
